cc064eb9a6e4a1430f6ca5d1d25d5b933e6aa07e
lalit wadekar <lalit.wadekr@gmail.com>
2025-02-02 02:10:55 +0530

    .

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ function clearScreen() {
   document.getElementById("result").value = "";
 }
 
+
 // This function display values
 function display(value) {
   document.getElementById("result").value += value;
